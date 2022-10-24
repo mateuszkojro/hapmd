8efdde69960979c6c84f1ed4ebae9416d6dcb56c
piotr233 <pdrabik223@gmail.com>
2022-10-24 15:05:54 +0200

    simple panel with basic information

--- src/app.py
+++ src/app.py
@@ -1,58 +1,100 @@
-from dash import Dash, html
+import time
+from dash import Dash, html, dcc
+from dash.dependencies import Output, Input
 
 app = Dash(__name__)
-# app.css.append_css({'external_url': '/assets/style.css'})
-
-
-colors = {"background": "#262626", "text": "#7FDBFF", "panel":"#363636"}
-
 
 app.layout = html.Div(
-    style={"backgroundColor": colors["background"]},
-    className='device_info',
+    className="device_info",
     children=[
+        dcc.Location(id="url", refresh=False),
         html.H1(
             children="Hampd",
-            className = "header_text",
+            className="header_text",
         ),
         html.H5(
             children="Horizontal antenna pattern measurement device",
-            className = "header_text",
+            className="header_text",
         ),
         html.Div(
-            style={"backgroundColor": colors["background"], "display":"table",},
+            className="device_grid",
             children=[
                 html.Div(
-                    className = "device_info",
+                    className="device_info",
                     children=[
-                        html.H4(
-                            children="HAMEG HMS3010",
-                            className = "text",
-                        ),
-                        html.H4(
-                            children="Connection: ok",
-                            className = "text"
-                        ),
-                        html.H4(
-                            children="State: ready",
-                            className = "text",
+                        html.H2("HAMEG HMS3010", className="header_text"),
+                        html.Table(
+                            [
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("Connection")),
+                                        html.Td(html.P(id="hameg_connection")),
+                                        html.Td(html.H3("Sweep Min Frequency")),
+                                        html.Td(html.P(id="hameg_sweep_min_frequency")),
+                                    ],
+                                ),
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("State")),
+                                        html.Td(html.P(id="hameg_state")),
+                                        html.Td(html.H3("Sweep max Frequency")),
+                                        html.Td(html.P(id="hameg_sweep_max_frequency")),
+                                    ],
+                                ),
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("IDN")),
+                                        html.Td(html.P(id="hameg_idn")),
+                                        html.Td(html.H3("Frequency step")),
+                                        html.Td(html.P(id="hameg_frequency_step")),
+                                    ],
+                                ),
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("Sweep Time")),
+                                        html.Td(html.P(id="hameg_sweep_time")),
+                                        html.Td(html.H3("Measurement Time")),
+                                        html.Td(html.P(id="hameg_measurement_time")),
+                                    ]
+                                ),
+                            ]
                         ),
                     ],
                 ),
                 html.Div(
                     className="device_info",
                     children=[
-                        html.H3(
-                            children="Rotor device",
-                            className = "text",
+                        html.H2(
+                            "Rotor device",
+                            className="header_text",
                         ),
-                        html.H4(
-                            children="Connection: ok",
-                            className = "text",
-                        ),
-                        html.H4(
-                            children="State: ready",
-                            className = "text",
+                        html.Table(
+                            [
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("Connection")),
+                                        html.Td(html.P(id="rotor_connection")),
+                                        html.Td(html.H3("Min angle")),
+                                        html.Td(html.P(id="rotor_min_angle")),
+                                    ]
+                                ),
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("State")),
+                                        html.Td(html.P(id="rotor_state")),
+                                        html.Td(html.H3("Max angle")),
+                                        html.Td(html.P(id="rotor_max_angle")),
+                                    ]
+                                ),
+                                html.Tr(
+                                    [
+                                        html.Td(html.H3("Angle")),
+                                        html.Td(html.P(id="rotor_angle")),
+                                        html.Td(html.H3("Angle Step")),
+                                        html.Td(html.P(id="rotor_angle_step")),
+                                    ]
+                                ),
+                            ]
                         ),
                     ],
                 ),
@@ -60,6 +102,65 @@ app.layout = html.Div(
         ),
     ],
 )
+
+
+@app.callback(
+    Output("hameg_connection", "children"),
+    Output("hameg_state", "children"),
+    Output("hameg_idn", "children"),
+    Output("hameg_sweep_time", "children"),
+    Output("hameg_measurement_time", "children"),
+    Output("hameg_sweep_min_frequency", "children"),
+    Output("hameg_sweep_max_frequency", "children"),
+    Output("hameg_frequency_step", "children"),
+    Output("rotor_connection", "children"),
+    Output("rotor_state", "children"),
+    Output("rotor_angle", "children"),
+    Output("rotor_min_angle", "children"),
+    Output("rotor_max_angle", "children"),
+    Output("rotor_angle_step", "children"),
+    Input("url", "pathname"),
+)
+def callback_a(x):
+    hameg_connection = "Ok"
+    hameg_state = "Ready"
+    hameg_idn = f"idn stuff {12334.545345}"
+    hameg_sweep_time = 0.2
+    hameg_sweep_min_frequency = 1*10**6
+    hameg_sweep_max_frequency = 8*10**6
+    hameg_frequency_step = 100
+    rotor_connection = "Ok"
+    rotor_state = "Redy"
+    rotor_angle = 12.4
+    rotor_min_angle = 90
+    rotor_max_angle = 270
+    rotor_angle_step = 0.2
+
+    hameg_measurement_time = (
+        hameg_sweep_time * (hameg_sweep_max_frequency - hameg_sweep_min_frequency)
+    ) / hameg_frequency_step
+
+    #TODO better representation of measurement time, convert seconds to hours and minutes 
+    #TODO nicer table, with breaking lines and light frame
+    #TODO find out more parameters that need to be displayed 
+     
+    return (
+        hameg_connection,
+        hameg_state,
+        hameg_idn,
+        f"{hameg_sweep_time}s",
+        f"{hameg_measurement_time / (60*60)}h",
+        f"{hameg_sweep_min_frequency}Hz",
+        f"{hameg_sweep_max_frequency}Hz",
+        f"{hameg_frequency_step}Hz",
+        rotor_connection,
+        rotor_state,
+        rotor_angle,
+        rotor_min_angle,
+        rotor_max_angle,
+        rotor_angle_step,
+    )
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
