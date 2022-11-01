ebac4a32236815a8c7a7f1807547cd4f086ed872
piotr233 <pdrabik223@gmail.com>
2022-11-01 19:55:36 +0100

    working plot generator, still has bugs tho

--- src/polar_plot.py/plot.py
+++ src/polar_plot.py/plot.py
@@ -1,18 +1,22 @@
 # import numpy and matplotlib library
+from email import message
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+measurement = pd.read_csv("output.csv")
+measurement = measurement.set_index(measurement.columns[0])
+
+max_val = measurement.max()
+max_val = measurement.min()
 
 plt.axes(projection="polar")
+# plt.set_theta_zero_lsocation("N")
 
+for angle in measurement.index:
+    for freq in measurement.columns:
+        print(angle, freq, measurement.at[angle,freq])
+        plt.scatter(angle, freq, c = (measurement.at[angle,freq] + 1) / 2, s=0.2, cmap='hsv')
 
-rads = np.arange(0, (2 * np.pi), 0.01)
-rs = np.arange(0, (2 * np.pi), 0.01)
-
-
-# plotting the circle
-for i, r in zip(rads, rs):
-    plt.polar(i, r, "g.")
-
-# display the Polar plot
+# # display the Polar plot
 plt.savefig("polat_plot.pdf")
