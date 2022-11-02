eba04089cab2a077ea699858e1ade6b2281ebb7e
piotr233 <pdrabik223@gmail.com>
2022-11-02 18:39:31 +0100

    working plotter

--- src/polar_plot.py/plot.py
+++ src/polar_plot.py/plot.py
@@ -1,22 +1,35 @@
 # import numpy and matplotlib library
-from email import message
-import numpy as np
+import math
 import matplotlib.pyplot as plt
 import pandas as pd
 
 measurement = pd.read_csv("output.csv")
 measurement = measurement.set_index(measurement.columns[0])
 
-max_val = measurement.max()
-max_val = measurement.min()
 
-plt.axes(projection="polar")
+plt.axes(projection="polar",)
 # plt.set_theta_zero_lsocation("N")
+fig = plt.figure()
+ax = fig.add_subplot(projection='polar')
 
+ax.set_theta_zero_location('N')
 for angle in measurement.index:
     for freq in measurement.columns:
-        print(angle, freq, measurement.at[angle,freq])
-        plt.scatter(angle, freq, c = (measurement.at[angle,freq] + 1) / 2, s=0.2, cmap='hsv')
+        if angle < 0:
+            theta = 360 + angle
+        else:
+            theta = angle
+        theta = math.radians(theta)
+        ax.scatter(theta, freq, c = ((measurement.at[angle,freq] + 1) / 2), s=0.5, cmap='plasma', alpha = 0.2)
+
+
+no_labels = len(ax.yaxis.get_ticklabels())
+for n, label in enumerate(ax.yaxis.get_ticklabels()):
+    if n % (no_labels//5) != 0:
+        label.set_visible(False)
+        
+
+ax.yaxis.get_ticklabels()[-1].set_visible(True)
 
 # # display the Polar plot
-plt.savefig("polat_plot.pdf")
+fig.savefig("polat_plot.pdf")
