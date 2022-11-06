4257578f10ef6b2343be06cd9257785e3c2fbe18
piotr233 <pdrabik223@gmail.com>
2022-11-06 10:35:59 +0100

    working plotter

--- src/polar_plot.py/plot.py
+++ src/polar_plot.py/plot.py
@@ -2,6 +2,8 @@
 import math
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+from matplotlib.pyplot import cm
 
 measurement = pd.read_csv("output.csv")
 measurement = measurement.set_index(measurement.columns[0])
@@ -12,22 +14,24 @@ plt.axes(projection="polar",)
 fig = plt.figure()
 ax = fig.add_subplot(projection='polar')
 
+color = iter(cm.rainbow(np.linspace(0, 1, len(measurement.columns))))
+
 ax.set_theta_zero_location('N')
-for angle in measurement.index:
-    for freq in measurement.columns:
+for idx, freq in enumerate(measurement.columns):
+    current_color = next(color)
+    for angle in measurement.index:
         if angle < 0:
             theta = 360 + angle
         else:
             theta = angle
         theta = math.radians(theta)
-        ax.scatter(theta, freq, c = ((measurement.at[angle,freq] + 1) / 2), s=0.5, cmap='plasma', alpha = 0.2)
+        ax.scatter(theta, measurement.at[angle,freq], color = current_color, s=0.5,  alpha = 0.4)
 
 
 no_labels = len(ax.yaxis.get_ticklabels())
 for n, label in enumerate(ax.yaxis.get_ticklabels()):
     if n % (no_labels//5) != 0:
-        label.set_visible(False)
-        
+        label.set_visible(False)        
 
 ax.yaxis.get_ticklabels()[-1].set_visible(True)
 
