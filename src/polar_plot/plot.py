a7b75ba7cc52c3ee842c04fde65efcf0bf8ceb85
piotr233 <pdrabik223@gmail.com>
2022-11-07 10:22:11 +0100

    reformatting code to be more readable

---
+++ src/polar_plot/plot.py
@@ -0,0 +1,88 @@
+# import numpy and matplotlib library
+import math
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import cm
+import pandas as pd
+import numpy as np
+import sys
+
+if __name__ == "__main__":
+
+    hapmd_settings_file = None
+
+    if len(sys.argv) == 2:
+        hapmd_settings_file = str(sys.argv[1])
+    else:
+        raise ValueError(f"Provided file: '{hapmd_settings_file}' is incorrect")
+
+    if hapmd_settings_file[-4:] != ".csv":
+        raise ValueError(
+            f"Provided file must have extension '.csv' not '{hapmd_settings_file[-4:]}'"
+        )
+
+    measurement: pd.DataFrame = pd.read_csv(hapmd_settings_file)
+    measurement = measurement.set_index(measurement.columns[0])
+
+    plt.axes(
+        projection="polar",
+    )
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="polar")
+    ax.set_theta_zero_location("N")
+    ax.set_theta_direction(-1)
+
+    color = iter(cm.rainbow(np.linspace(0, 1, len(measurement.columns))))
+
+    if len(measurement.columns) == 1:
+        alpha = 1
+    else:
+        alpha = 0.6
+
+    ax.scatter(
+        0,
+        0,
+        alpha=0,
+    )
+    ax.scatter(
+        0,
+        -100,
+        alpha=0,
+    )
+
+    measurement_index_as_radians = [math.radians(freq) for freq in measurement.index]
+
+    for idx, freq in enumerate(measurement.columns):
+        current_color = next(color)
+        ax.scatter(
+            measurement_index_as_radians,
+            measurement.iloc[:, idx].values,
+            color=current_color,
+            s=0.5,
+            alpha=alpha,
+            label=f"{'{:.3e}'.format(float(freq))}Hz",
+        )
+
+    no_labels = len(ax.yaxis.get_ticklabels())
+    for n, label in enumerate(ax.yaxis.get_ticklabels()):
+        if n % (no_labels // 5) != 0:
+            label.set_visible(False)
+                
+    ax.yaxis.get_ticklabels()[-1].set_visible(True)
+    
+    
+    (lines, labels) = plt.thetagrids(
+        range(0, 360, 45), ("0°", "45°", "90°", "135°", "180°", "-135°", "-90°", "-45°")
+    )
+
+    for label, angle in zip(labels, range(0, 360, 45)):
+        label.set_rotation(90 - angle)
+
+    # label_position=ax.get_rlabel_position()
+    # ax.text(np.radians(label_position+10),ax.get_rmax()/2.,'Antenna signal Strength [dB]',
+    #     rotation=label_position,ha='center',va='center')
+
+
+    ax.set_title("Horizontal Antenna Pattern measurement for Frequency")
+    ax.legend(loc="lower right")
+    # # display the Polar plot
+    fig.savefig(hapmd_settings_file[:-4] + "_plot.pdf")
