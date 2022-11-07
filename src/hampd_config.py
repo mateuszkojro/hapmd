2275933679dface39c23cae234a9cfa47f90dd50
piotr233 <pdrabik223@gmail.com>
2022-11-07 10:21:34 +0100

    formatted with black

--- src/hampd_config.py
+++ src/hampd_config.py
@@ -34,47 +34,68 @@ class HapmdConfig(BaseModel):
         config = HapmdConfig(**data)
         config.config_json_file_path = file_path
         return config
-    
+
     @root_validator()
-    def calculate_measured_frequencies(cls,values):
-        if values.get('hameg_frequencies') is not None:
+    def calculate_measured_frequencies(cls, values):
+        if values.get("hameg_frequencies") is not None:
             values["hameg_sweep_min_frequency"] = 0
             values["hameg_sweep_max_frequency"] = 0
             values["hameg_frequency_step"] = 0
             return values
-        
+
         hameg_sweep_min_frequency = values.get("hameg_sweep_min_frequency")
         hameg_sweep_max_frequency = values.get("hameg_sweep_max_frequency")
         hameg_frequency_step = values.get("hameg_frequency_step")
-        
-        hameg_frequencies = [freq for freq in range(hameg_sweep_min_frequency,hameg_sweep_max_frequency,hameg_frequency_step)]
-        
+
+        hameg_frequencies = [
+            freq
+            for freq in range(
+                hameg_sweep_min_frequency,
+                hameg_sweep_max_frequency,
+                hameg_frequency_step,
+            )
+        ]
+
         values["hameg_frequencies"] = hameg_frequencies
-        
+
         return values
-         
+
     def print_config(self: "HapmdConfig"):
         print(
             f"""
 Connecting Hameg Device on VID: {Colors.BOLD}{hex(self.hameg_vid)}{Colors.ENDC} 
 Connecting Hameg Device on PID: {Colors.BOLD}{hex(self.hameg_pid)}{Colors.ENDC}
-Sweep Time: {Colors.BOLD}{self.hameg_sweep_time}{Colors.ENDC} s""")
-        
-        print(f"""
-Frequency Step: {Colors.BOLD}{self.hameg_frequency_step}{Colors.ENDC} Hz""")
-        print(f"""
+Sweep Time: {Colors.BOLD}{self.hameg_sweep_time}{Colors.ENDC} s"""
+        )
+
+        print(
+            f"""
+Frequency Step: {Colors.BOLD}{self.hameg_frequency_step}{Colors.ENDC} Hz"""
+        )
+        print(
+            f"""
 Sweep Start Frequency: {Colors.BOLD}{'{:.3e}'.format(self.hameg_sweep_min_frequency)}{Colors.ENDC} Hz 
-Sweep Stop Frequency: {Colors.BOLD}{'{:.3e}'.format(self.hameg_sweep_max_frequency)}{Colors.ENDC} Hz""")
+Sweep Stop Frequency: {Colors.BOLD}{'{:.3e}'.format(self.hameg_sweep_max_frequency)}{Colors.ENDC} Hz"""
+        )
 
         if self.hameg_frequencies is not None:
-            
+
             print(f"List of measured frequencies:")
             for f in self.hameg_frequencies:
-                print('\t\t\t\t'+Colors.BOLD + '{:.3e}'.format(f)+Colors.ENDC+'Hz'+Colors.ENDC)
+                print(
+                    "\t\t\t\t"
+                    + Colors.BOLD
+                    + "{:.3e}".format(f)
+                    + Colors.ENDC
+                    + "Hz"
+                    + Colors.ENDC
+                )
 
-        print(f"""
+        print(
+            f"""
 Connecting Rotor Device on Com port: {Colors.BOLD}{self.rotor_com_port}{Colors.ENDC}
 Rotor Step Angle: {Colors.BOLD}{self.rotor_angle_step}{Colors.ENDC}
 Rotor Start Angle: {Colors.BOLD}{self.rotor_min_angle}{Colors.ENDC}          
 Stop Angle: {Colors.BOLD}{self.rotor_min_angle}{Colors.ENDC}
-""")
+"""
+        )
